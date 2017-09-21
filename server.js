d1b15b05f106b6e07b7098939dd208952892ce91
km <453013645@qq.com>
2017-09-21 16:07:37 +0800

    合并冲突

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-
 /*
 last-update:cjm
 update:cjm
 
 */
-
 //服务器,设置静态文件夹
 var express = require('express');
 var mysql = require('mysql');
@@ -17,7 +15,6 @@ app.listen(12345);
 
 
 //引入自定义模块
-
 var handle = require('./CMS/api/sql_caozuo.js');
 handle(app,mysql,bodyParse);
 	
@@ -50,6 +47,5 @@ goods(app,connect.connection);
 //指定根目录下的pubilc文件夹作为静态资源的存放地方(css,js,html,json,jpg)
 app.use(express.static('src'));
 app.use(express.static('CMS'));
-
 
 console.log("开启服务器")
